3af2d7a536f915cab7cc848bda04c66f0067b84e
Vijay <blumartrepository@gmail.com>
2024-06-15 15:37:01 +0530

    Initial Push

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/get",(req,res)=>{
+    res.send({"message":"My first Node js Application"});
+})
+
+app.listen(8000,()=>{
+    console.log("Listening to 8000");
+})
