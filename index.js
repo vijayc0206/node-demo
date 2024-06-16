60ce37b398f61c24ed4928d16ca4e1378cf59702
Vijay <blumartrepository@gmail.com>
2024-06-16 21:57:45 +0530

    env changes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-
+require("dotenv").config();
+const port = 3000;
 
 app.get("/api/get",(req,res)=>{
     res.send({"message":"My first Node js Application"});
 })
 
-app.listen(8000,()=>{
-    console.log("Listening to 8000");
+app.get("/api/getData",(req,res)=>{
+    res.send({"message":"${process.env.name} first Node js project"});
+})
+
+app.listen(port,()=>{
+    console.log("Listening to ${port}");
 })
